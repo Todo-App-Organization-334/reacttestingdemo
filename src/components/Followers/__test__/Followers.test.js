e31a85eedb7b2ecde1617b86431dca57cab2cb4e
nam334 <namratadas334@gmail.com>
2025-03-27 13:00:25 +0530

    setup completed for msw

---
+++ src/components/Followers/__test__/Followers.test.js
@@ -0,0 +1,19 @@
+import { render, screen } from "@testing-library/react";
+import Followers from "../Followers";
+import { BrowserRouter } from "react-router-dom";
+import React from "react";
+
+const MockFollowerComponent = () => (
+  <BrowserRouter>
+    <Followers />
+  </BrowserRouter>
+);
+describe("Unit Tests (Component Behavior)", () => {
+  it("Should render the Header component with the correct title.", () => {
+    render(<MockFollowerComponent />);
+    const headingElement = screen.getByRole("heading");
+    expect(headingElement.value).toHaveTextContent("Followers");
+  });
+
+  it("Should render the FollowersList component.", () => {});
+});
--- src/setupTests.js
+++ src/setupTests.js
@@ -2,4 +2,15 @@
 // allows you to do things like:
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
-import '@testing-library/jest-dom';
+import "@testing-library/jest-dom";
+import { server } from "./mocks/server";
+import { afterAll, afterEach, beforeAll } from "@jest/globals";
+
+// Start the mock server before running tests
+beforeAll(() => server.listen());
+
+// Reset handlers after each test (to prevent state leaks)
+afterEach(() => server.resetHandlers());
+
+// Close the server when tests are done
+afterAll(() => server.close());
