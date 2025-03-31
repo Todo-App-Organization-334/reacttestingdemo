7ae59a6dd9306e15e38b6fd15bb7ba3cd665c55c
nam334 <namratadas334@gmail.com>
2025-03-31 14:08:34 +0530

    added changes 23

--- jest.config.js
+++ jest.config.js
@@ -1,34 +1,31 @@
 module.exports = {
   testEnvironment: "jsdom",
-  testMatch: ["**/src/components/**/__tests__/**/*.test.js"],
   testPathIgnorePatterns: ["/node_modules/", "/public/"],
   setupFilesAfterEnv: ["@testing-library/jest-dom/extend-expect"],
 
   collectCoverage: true,
   collectCoverageFrom: [
-    "src/components/**/*.js",
-    "!src/**/__tests__/**", // Exclude test files from coverage calculation
-    "!src/index.js",
-    "!src/reportWebVitals.js",
+    "src/components/AddInput/**/*.js",
+    "src/components/Todo/**/*.js",
+    "src/components/Followers/**/*.js",
+    "src/components/FollowersList/**/*.js",
   ],
+
   coverageThreshold: {
     global: {
-      statements: 0, // No global threshold
-      branches: 0,
-      functions: 0,
-      lines: 0,
-    },
-    "./src/**/__tests__/**/*.test.js": {
       statements: 90,
       branches: 90,
       functions: 90,
       lines: 90,
     },
   },
+
   coverageDirectory: "coverage",
   coverageReporters: ["json", "lcov", "text", "clover"],
+
   moduleNameMapper: {
     "\\.(css|less|scss|sass)$": "identity-obj-proxy",
   },
+
   transformIgnorePatterns: ["node_modules/(?!axios)/"],
 };
