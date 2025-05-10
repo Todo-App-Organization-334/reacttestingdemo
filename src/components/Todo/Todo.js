b4a9f278880400a094554860cd0fe430cb4f9382
namratadas888 <namratadas888@gmail.com>
2025-05-10 22:05:11 +0530

    This is a commit message test cicd workflow.

--- src/components/Todo/Todo.js
+++ src/components/Todo/Todo.js
@@ -9,7 +9,7 @@ function Todo() {
 
   return (
     <div className="todo">
-      <Header title="TODO LIST UPDATED" />
+      <Header title="TODO LIST" />
       <AddInput setTodos={setTodos} todos={todos} />
       <TodoList todos={todos} setTodos={setTodos} />
     </div>
